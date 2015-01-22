39facd328fdb5173c9db097e28a6dcfae6483895
Cătălin Bogdan <ionut.bogdan@info.uaic.ro>
2015-01-22 22:44:33 +0200

    Using cookies to remember if in history or bookmarks mode and now starting history search when pressed enter

--- js/attachEvents.js
+++ js/attachEvents.js
@@ -1,14 +1,37 @@
 $(document).ready(function() {
-    // When loading document for the first time, attach history related events
-    attachHistoryEvents();
-
-    // And also load history UI components
-    // TODO: save filter/search/mode(bookmark/history) values in cookies and restore them here
-
-    loadHistoryItems(descCompareHistoryDate,'');
+    // Cookies !
+    if (document.cookie.indexOf('mode') == -1) document.cookie = "mode=history";
 
     // Add event for bookmark switch
     $('#switchHistoryBookmarks').on("click",switchBookmarkHistory);
+
+    if (getCookie('mode').indexOf('history') > -1)
+    {
+        // When loading document for the first time, attach history related events
+        attachHistoryEvents();
+
+        // And also load history UI components
+        loadHistoryItems(descCompareHistoryDate, '');
+    }
+
+
+    if (getCookie('mode').indexOf('bookmarks') > -1)
+    {
+        var button = document.getElementById ('switchHistoryBookmarks');
+        button.innerHTML="Switch to History";
+
+        // remove graphical history elements
+        deleteHistoryViewItems();
+
+        // Attach bookmark events
+        attachBookmarkEvents();
+
+        // Load bookmark UI
+        loadBookmarkItems(descCompareBookmarkDate,'');
+
+        // Change UI to Bookmark mode
+        changeUiForBookmarksMode();
+    }
 
     // Change alertify default title
     alertify.defaults.glossary.title = 'Toryvis';
@@ -17,8 +40,13 @@ $(document).ready(function() {
     // Used to smoothen animations by calling removeElement
     // when animations are complete.
     document.body.addEventListener('webkitAnimationEnd', removeElement);
-});
 
+
+    // Since the call_back page cannot use the Chrome API directly,
+    // extracted bookmarks will be stored using the local storage,
+    // then processed through the Bookmark API, and the deleted
+    $(window).on('storage', changedLocalStorage);
+});
 
 /**
  *  Attaches history-related functions to events
@@ -34,7 +62,7 @@ function attachHistoryEvents ()
     $("#ascOrDesc").off ("change",sortBookmarkItems);
 
     // Event for searching
-    $("#searchButton").off ("click",sortBookmarkItems);
+    $("#searchButton").off ("keyup", onSearchKeyUpBookmarks);
     $("#searchText").off ("input", sortBookmarkItems);
 
 
@@ -49,7 +77,7 @@ function attachHistoryEvents ()
 
     // Delete button function
     $("#deleteButton").on ("click",deleteHistoryItems);
-    $("#searchText").on ("input", sortHistoryItems);
+    $("#searchText").on ("keyup", onSearchKeyUpHistory);
 }
 
 function attachBookmarkEvents ()
@@ -64,7 +92,7 @@ function attachBookmarkEvents ()
 
     // Delete button function
     $("#deleteButton").off ("click",deleteHistoryItems);
-    $("#searchText").off ("input", sortHistoryItems);
+    $("#searchText").off ("keyup", onSearchKeyUpHistory);
 
 
 
@@ -78,5 +106,34 @@ function attachBookmarkEvents ()
 
     // Event for searching
     $("#searchButton").on ("click",sortBookmarkItems);
-    $("#searchText").on ("input", sortBookmarkItems);
+    $("#searchText").on ("keyup", onSearchKeyUpBookmarks);
+}
+
+function onSearchKeyUpBookmarks (e)
+{
+    if (e.keyCode ==13)
+    {
+        sortBookmarkItems();
+    }
+}
+
+function onSearchKeyUpHistory (e)
+{
+    if (e.keyCode ==13)
+    {
+        sortHistoryItems();
+    }
+}
+
+/**
+ * Extracts string from cookie on a key=value pair basis.
+ *
+ * @param name key
+ * @returns {*} value
+ */
+function getCookie(name)
+{
+    var re = new RegExp(name + "=([^;]+)");
+    var value = re.exec(document.cookie);
+    return (value != null) ? unescape(value[1]) : null;
 }
